4313b9f8a6f128f0fa6a7aab93e4651c9d8ddd8a
atwrx <act.webdesigns@gmail.com>
2019-09-29 09:58:11 -0500

    deleted warning, prepped for boilerplate cloning

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
 
-const Warning = React.lazy(() => import('./Warning'))
-
 class App extends React.Component {
   state = {
     count: 0
@@ -11,24 +9,15 @@ class App extends React.Component {
   increment = () => {
     this.setState(state => ({ count: state.count + 1 }))
   }
- 
+
   decrement = () => {
     this.setState(state => ({ count: state.count - 1 }))
   }
 
   render() {
-    const { count } = this.state
     return (
-      <div role='main'>
+      <div role="main">
         <h1>Hello World</h1>
-        <h2 className={count >= 5 ? 'warning' : null}>Count: {count}</h2>
-        <button onClick={this.increment}>+</button>
-        <button onClick={this.decrement}>-</button>
-        {count > 4 ? (
-          <React.Suspense fallback={null}>
-            <Warning />
-          </React.Suspense>
-        ) : null}
       </div>
     )
   }
--- src/Warning.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Warning = () => <span className={'warning'}>Chill out bruh</span>
-
-Warning.displayName = 'Warning'
-
-export default Warning
